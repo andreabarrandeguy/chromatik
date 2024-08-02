e08d4c82b9f6e576b911a2a21f702c2ac0d9d819
Andrea Barrandeguy <abarrandeguy87@hotmail.com>
2024-08-02 01:08:49 -0300

    Fixed phone vertical photos handling

--- filters/views.py
+++ filters/views.py
@@ -1,12 +1,36 @@
 import os
 from django.shortcuts import render
-from PIL import Image
+from PIL import Image, ExifTags
 from django.core.files.storage import default_storage
 import io
 import base64
 from .filters import apply_purple_filter, apply_turquoise_filter, apply_red_filter
 
 # Create your views here.
+def correct_image_orientation(image):
+    try:
+        for orientation in ExifTags.TAGS.keys():
+            if ExifTags.TAGS[orientation] == 'Orientation':
+                break
+
+        exif = image._getexif()
+
+        if exif is not None:
+            orientation = exif.get(orientation)
+
+            if orientation == 3:
+                image = image.rotate(180, expand=True)
+            elif orientation == 6:
+                image = image.rotate(270, expand=True)
+            elif orientation == 8:
+                image = image.rotate(90, expand=True)
+
+    except (AttributeError, KeyError, IndexError):
+        # cases: image don't have getexif
+        pass
+
+    return image
+
 def index(request):
     if request.method == "POST":
         input_file = request.FILES.get('input_file')
@@ -17,10 +41,13 @@ def index(request):
 
         filter_type = request.POST.get("filter")
         ogimg = Image.open('./filters/static/filters/input.jpg')
+
+        # Correct the orientation
+        ogimg = correct_image_orientation(ogimg)
         
         # Resize original image for process performance
         ogwidth, ogheight = ogimg.size
-        newsize = (int((ogwidth/ogheight)*750), 750)
+        newsize = (int((ogwidth / ogheight) * 750), 750)
         img = ogimg.resize(newsize)
 
         # Apply the appropriate filter
@@ -38,7 +65,7 @@ def index(request):
         new_img.save(bufferOutput, 'PNG')
         PNG = bufferOutput.getvalue()
         output = base64.b64encode(PNG).decode("utf-8")
-        os.remove('./filters/static/filters/input.jpg')
+        os.remove(imgSaved)
 
         if img.width >= img.height:
             return render(request, "filters/outputh.html", {"output": output})
